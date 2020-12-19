c4d0969a398e1549b92b551a42d8c324767d5220
Felix de Perthuis <fdeperthuis@sparkling-partners.com>
2020-12-19 13:49:45 +0100

    first front

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'chair', to: 'pages#chair'
+  get 'story', to: 'pages#story'
+  get 'stores', to: 'pages#stores'
 end
